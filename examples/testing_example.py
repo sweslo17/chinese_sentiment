a4e23327380ed587f7ecadecaf0462c36081e9cd
sweslo17 <sweslo17@gmail.com>
2015-05-28 17:15:21 +0800

    修改測試範例

--- examples/testing_example.py
+++ examples/testing_example.py
@@ -4,7 +4,7 @@ sys.path.append('../src')
 import testing
 
 testing.load_training_data('../model/','../dict/user_dic.dic')
-result = testing.test_sentance('民進黨的政治學者，想去大陸出賣台灣，結果被拒絕了，悲哀~')
+result = testing.test_sentance('沒做功課就來討罵')
 if result['pos'] > result['neg']:
 	print 'positive'
 elif result['neg'] > result['pos']:
